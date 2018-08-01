fd9ceb3b0fb92dcd2f7bed4e11e9f491261c799e
Hugo Escandon <hescandon6@gmail.com>
2018-08-01 17:18:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,16 @@
 def reformat_languages(languages)
   new_hash = {}
 
+  languages.each do |style,language|
+    language.each do |name,type|
+        if new_hash[name] == nil
+          new_hash[name] = type
+          new_hash[name][:style] = []
+        end
+        new_hash[name][:style] << style
+    end
+  end
+
   # your code here
 end
 new_hash
